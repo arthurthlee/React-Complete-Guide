571f4b7cc033e09a721440f9d42f6c039424571a
Arthur Lee <ruhtraeel@gmail.com>
2019-08-26 13:12:40 -0700

    Chapter 4 Lesson 9

--- src/App.js
+++ src/App.js
@@ -25,12 +25,24 @@ class App extends Component {
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons:[
-      {name: 'Max', age: 28},
-      {name: event.target.value, age: 29},
-      {name: 'Stephanie', age: 26},
-    ]})
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+
   }
 
   render() {
@@ -52,7 +64,8 @@ class App extends Component {
               key={person.id}
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age}/>
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
