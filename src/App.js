babb48e335ec4b48509326e7f014175dbe015cad
Arthur Lee <ruhtraeel@gmail.com>
2019-08-26 12:55:25 -0700

    Chapter 4 Lesson 6

--- src/App.js
+++ src/App.js
@@ -18,14 +18,10 @@ class App extends Component {
     this.setState({showPersons: !doesShow})
   }
 
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    //console.log('Was clicked!');
-    this.setState({persons:[
-      {name: 'Max', age: 28},
-      {name: newName, age: 29},
-      {name: 'Stephanie', age: 27},
-    ]})
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   nameChangedHandler = (event) => {
@@ -50,8 +46,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}/>
           })}
