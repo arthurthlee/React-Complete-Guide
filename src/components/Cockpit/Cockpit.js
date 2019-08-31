3394990822782e61aa240e31628043c7250c98fa
Arthur Lee <ruhtraeel@gmail.com>
2019-08-31 03:08:24 -0700

    Chapter 7 Lesson 10

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './Cockpit.css'
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // Http request...
+    });
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
@@ -26,4 +30,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default Cockpit;
