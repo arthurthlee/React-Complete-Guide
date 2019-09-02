1540f44242e59733582085ce197c2368f2029ed7
Arthur Lee <ruhtraeel@gmail.com>
2019-09-02 00:18:18 -0700

    Chapter 7 Lesson 23: Another form of Higher Order Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Auxiliary from '../hoc/Auxiliary';
 
 class App extends Component {
   constructor(props) {
@@ -94,7 +95,7 @@ class App extends Component {
 
 
     return (
-      <WithClass classes={classes.App}>
+      <Auxiliary>
         <button
           onClick={() => {
               this.setState({showCockpit: false});
@@ -108,11 +109,11 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         /> : null}
         {persons}
-      </WithClass>
+      </Auxiliary>
 
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
