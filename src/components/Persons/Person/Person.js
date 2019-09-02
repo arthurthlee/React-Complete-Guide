207c3a6544ee2fc242f6905a4ae3528e3b24a2f2
Arthur Lee <ruhtraeel@gmail.com>
2019-09-02 00:00:59 -0700

    Chapter 7 Lesson 19

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../hoc/Auxiliary';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.clicked}>
-                    I'm {this.props.name} and I am {this.props.age} years old!
-                </p>
-                <p>{this.props.children}</p>
-                <input
-                    type="text"
-                    onChange={this.props.changed}
-                    value={this.props.name}/>
-            </div>
+            <Aux>
+                    <p onClick={this.props.clicked}>
+                        I'm {this.props.name} and I am {this.props.age} years old!
+                    </p>
+                    <p>{this.props.children}</p>
+                    <input
+                        type="text"
+                        onChange={this.props.changed}
+                        value={this.props.name}/>
+
+            </Aux>
+
         );
     }
 }
